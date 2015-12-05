3b6fc73446d217c93ae8964804a007711ea09af3
Tahrim <utahrim@gmail.com>
2015-12-05 12:43:10 -0500

    Add migrations for bins

---
+++ db/migrate/20151205171953_create_bins.rb
@@ -0,0 +1,13 @@
+class CreateBins < ActiveRecord::Migration
+  def change
+    create_table :bins do |t|
+      t.string :borough, null: false
+      t.string :site_type, null: false
+      t.string :site_name, null: false
+      t.string :address, null: false
+      t.float :latitude, null: false
+      t.float :longitude, null: false
+      t.timestamps null: false
+    end
+  end
+end
