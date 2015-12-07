c08beb1599f0aeeb27e670c5f44126de104d54cc
Andre Jesse <andre.isaac.jessee@gmail.com>
2015-12-07 17:49:28 -0500

    able to send user lat long from html5 to ruby controller

--- app/controllers/bins_controller.rb
+++ app/controllers/bins_controller.rb
@@ -8,4 +8,12 @@ class BinsController < ApplicationController
     @bins = Bin.all
     render "/bins/index"
   end
+
+  def getlatlng
+    @user_location = Geokit::LatLng.new(params[:lat].to_f, params[:lng].to_f)
+    @bin = Bin.closest(:origin => @user_location).first
+    binding.pry
+  end
+
+
 end
