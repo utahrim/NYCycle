590605a9d0e0db7e078587cb42ed0eec91a864f1
Andre Jesse <andre.isaac.jessee@gmail.com>
2015-12-09 01:03:17 -0500

    map with markers and polygon line, walking directions, and street view all up and running. Need to create buttons to ajax these screens in.

--- app/controllers/bins_controller.rb
+++ app/controllers/bins_controller.rb
@@ -8,7 +8,18 @@ class BinsController < ApplicationController
   def getlatlng
     @user_location = Geokit::LatLng.new(params[:lat].to_f, params[:lng].to_f)
     @bin = Bin.closest(:origin => @user_location).first
-    render "/bins/walking_directions"
+    # render "/bins/walking_directions"
+    # render "/bins/street_view"
+  end
+
+   def walking_directions
+    @user_location = Geokit::LatLng.new(params[:lat].to_f, params[:lng].to_f)
+    @bin = Bin.closest(:origin => @user_location).first
+  end
+
+   def street_view
+    @user_location = Geokit::LatLng.new(params[:lat].to_f, params[:lng].to_f)
+    @bin = Bin.closest(:origin => @user_location).first
   end
 
   def convert_to_latlng
@@ -18,4 +29,5 @@ class BinsController < ApplicationController
     @bin = Bin.closest(:origin => @user_location).first
     render "/bins/getlatlng"
   end
+
 end
