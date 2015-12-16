f63be946a439f124e6718cbd774a31e3ca8111fa
Andre Jesse <andre.isaac.jessee@gmail.com>
2015-12-16 01:38:30 -0500

    indentation

--- app/assets/javascripts/user_geolocation.js
+++ app/assets/javascripts/user_geolocation.js
@@ -17,33 +17,36 @@ $(document).ready(function(){
       $('.address_form').fadeOut();
       $('.logo').animate({"margin-top": "8em"});
     }
-
   })
 
-   $('.success.button').on('click', function(event){
+  $('.success.button').on('click', function(event){
     event.preventDefault();
     $('.logo').children().animate({ "margin-left": ".5em" }, 100 )
     $('.logo').children().animate({ "height": "3em" }, 100 )
     $('.logo').animate({ "margin-top": "0em" }, 1000 );
     $('.main').animate({ "margin-top": "-4em" }, 1000 );
+
     var data = $(event.target).parent().serialize()
+
     $.ajax({
-    url: "/bins/convert_to_latlng",
-    method: "post",
-    data: data
+      url: "/bins/convert_to_latlng",
+      method: "post",
+      data: data
     })
     .done(function(response){
-    main.html(response);
+      main.html(response);
     });
   });
 
   $('.location').one('click', function(event){
 
     event.preventDefault();
+
     $('.logo').children().animate({ "margin-left": ".5em" }, 100 )
     $('.logo').children().animate({ "height": "3em" }, 100 )
     $('.logo').animate({ "margin-top": "0em" }, 1000 );
     $('.main').animate({ "margin-top": "-4em" }, 1000 );
+
     whereAmI();
 
     function whereAmI() {
@@ -66,21 +69,19 @@ $(document).ready(function(){
       };
 
       function error(error) {
-
         switch(error.code) {
-
-        case error.PERMISSION_DENIED:
-        errors.html("User denied the request for Geolocation.")
-        break;
-        case error.POSITION_UNAVAILABLE:
-        errors.html("Location information is unavailable.")
-        break;
-        case error.TIMEOUT:
-        errors.html("The request to get user location timed out.")
-        break;
-        case error.UNKNOWN_ERROR:
-        errors.html("An unknown error occurred.")
-        break;
+          case error.PERMISSION_DENIED:
+          errors.html("User denied the request for Geolocation.")
+          break;
+          case error.POSITION_UNAVAILABLE:
+          errors.html("Location information is unavailable.")
+          break;
+          case error.TIMEOUT:
+          errors.html("The request to get user location timed out.")
+          break;
+          case error.UNKNOWN_ERROR:
+          errors.html("An unknown error occurred.")
+          break;
         }
       };
 
@@ -89,42 +90,48 @@ $(document).ready(function(){
 
   })
 
-  $(document).on("submit", "#map-button", function(event){
+$(document).on("submit", "#map-button", function(event){
+
   event.preventDefault();
   var data = $(this).serialize();
+
   $.ajax({
     url: "/bins/getlatlng",
     method: "post",
     data: data
-    })
-    .done(function(response){
+  })
+  .done(function(response){
     main.html(response);
-    });
   });
+});
 
-  $(document).on("submit", "#walking-button", function(event){
+$(document).on("submit", "#walking-button", function(event){
+
   event.preventDefault();
   var data = $(this).serialize();
+
   $.ajax({
     url: "/bins/walking_directions",
     method: "post",
     data: data
-    })
-    .done(function(response){
+  })
+  .done(function(response){
     main.html(response);
-    });
   });
+});
 
-  $(document).on("submit", "#streetview-button", function(event){
+$(document).on("submit", "#streetview-button", function(event){
+
   event.preventDefault();
   var data = $(this).serialize();
+
   $.ajax({
     url: "/bins/street_view",
     method: "post",
     data: data
-    })
-    .done(function(response){
+  })
+  .done(function(response){
     main.html(response);
-    });
   });
+});
 })
